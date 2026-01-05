d93dea1f87850d86e04bd7312023e87923900d9d
Maynor David <134113042+Maynor06@users.noreply.github.com>
2026-01-04 18:44:35 -0600

    [FIX] mejoras de rendimiento

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
@@ -11,5 +13,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
